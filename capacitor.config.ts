3a6e934ec715b24fb93bf877e36981147d0044ff
Denis Perepelitsyn <fropsas@gmail.com>
2025-09-28 10:16:38 +0530

    feat: added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pay_hab.merchant.app',
+  appName: 'pay-hub-merchant',
+  webDir: 'dist'
+};
+
+export default config;
